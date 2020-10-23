e0bbdeef4aefc0d16863b84f4fa100f7cd169786
PDevgut <devgut.pavel@ya.ru>
2020-10-23 20:25:52 +0300

    16

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -1,13 +1,12 @@
 import mimetypes
+
 from framework.consts import DIR_STATIC
+
 
 def application(environ, start_response):
     url = environ["PATH_INFO"]
 
-    file_names = {
-        "/yyy": "styles.css",
-        "/bg.jpg/": "bg.jpg"
-    }
+    file_names = {"/yyy": "styles.css", "/bg.jpg/": "bg.jpg"}
 
     file_name = file_names.get(url, "index.html")
 
@@ -16,6 +15,7 @@ def application(environ, start_response):
     payload = read_static(file_name)
     start_response(status, list(headers.items()))
     yield payload
+
 
 # def read_from_index_html():
 #     path = DIR_STATIC / "index.html"  # Путь
@@ -38,7 +38,8 @@ def application(environ, start_response):
 #
 #     return payload
 
-def read_static(file_name:str) -> bytes:
+
+def read_static(file_name: str) -> bytes:
     path = DIR_STATIC / file_name  # Путь
     with path.open("rb") as fp:  # r - режим чтения, Открыть файл
         payload = fp.read()  # Чтение и запись
