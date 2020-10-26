14fb0c332ab820086b0222e102452a5fbc27cfc1
PDevgut <devgut.pavel@ya.ru>
2020-10-26 18:21:37 +0300

    19

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -1,4 +1,5 @@
 import mimetypes
+import random
 
 from framework.consts import DIR_STATIC
 
@@ -6,9 +7,9 @@ from framework.consts import DIR_STATIC
 def application(environ, start_response):
     url = environ["PATH_INFO"]
 
-    file_names = {"/yyy": "styles.css", "/bg.jpg/": "bg.jpg"}
+    file_names = {"/yyy": "styles.css", "/bg.jpg/": "bg.jpg", "/": "index.html"}
 
-    file_name = file_names.get(url, "index.html")
+    file_name = file_names.get(url)
 
     status = "200 OK"
     headers = {"Content-type": mimetypes.MimeTypes().guess_type(file_name)[0]}
@@ -44,3 +45,9 @@ def read_static(file_name: str) -> bytes:
     with path.open("rb") as fp:  # r - режим чтения, Открыть файл
         payload = fp.read()  # Чтение и запись
     return payload
+
+def generate_404(environ) -> bytes:
+    url = environ["PATH_INFO"]
+    pin = random.randint(1,1000)
+    msg = f"Hello world! Your path {url} not found. Pin {pin}"
+    return msg.encode()
