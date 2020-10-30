6ee428c6a3cc677a5bc642f52ab3e7646973617c
PDevgut <devgut.pavel@ya.ru>
2020-10-30 20:51:22 +0300

    23

--- src/framework/wsgi.py
+++ src/framework/wsgi.py
@@ -1,16 +1,16 @@
+from framework.types import ResponseT
 from handlers.bg import handle_bg
+from handlers.index import handle_index
 from handlers.not_found import handle_404
 from handlers.styles import handle_styles
-from hendlers.index import handle_index
 
 handle = {"/yyy": handle_styles, "/bg.jpg/": handle_bg, "/": handle_index}
+
 
 def application(environ, start_response):
 
     url = environ["PATH_INFO"]
     handle_name = handle.get(url, handle_404)
-    status, headers, payload = handle_name(environ)
-    start_response(status, list(headers.items()))
-    yield payload
-
-
+    ResponseT.status, ResponseT.headers, ResponseT.payload = handle_name(environ)
+    start_response(ResponseT.status, list(ResponseT.headers.items()))
+    yield ResponseT.payload
